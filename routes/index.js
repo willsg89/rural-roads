6beba34cd8c068390a1c4bf0539702c10220f049
William Guimarães <willsg89@gmail.com>
2018-07-17 22:59:39 -0300

    refatorando estrutura para ejs com includes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('main', {active: 'main'});
+});
+
+router.get('/tour', function(req, res, next) {
+  res.render('tour', {active: 'tour'});
+});
+
+router.get('/photos', function(req, res, next) {
+  res.render('photos', {active: 'photos'});
+});
+
+router.get('/videos', function(req, res, next) {
+  res.render('videos', {active: 'videos'});
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {active: 'contact'});
 });
 
 module.exports = router;
